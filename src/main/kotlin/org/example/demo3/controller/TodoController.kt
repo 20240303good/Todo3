0e408a422c7d0acd4860bc07dfb0a95d61867ffd
202040303good <trmf681@gmail.com>
2024-06-27 19:56:25 +0900

    TodoService

--- src/main/kotlin/org/example/demo3/controller/TodoController.kt
+++ src/main/kotlin/org/example/demo3/controller/TodoController.kt
@@ -1,17 +1,24 @@
 package org.example.demo3.controller
 
 import org.example.demo3.controller.request.CreateTodoRequest
+import org.example.demo3.service.TodoService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class TodoController {
+class TodoController(
+    val todoService: TodoService
+) {
     @PostMapping("/todos")
     fun CreateTodo(
         @RequestBody request : CreateTodoRequest,
     ): ResponseEntity<Boolean> {
-        return ResponseEntity.ok(true)
+        return ResponseEntity.ok(
+            todoService.createTodo(
+                request
+            )
+        )
     }
 }
---
+++ src/main/kotlin/org/example/demo3/service/TodoService.kt
@@ -0,0 +1,14 @@
+package org.example.demo3.service
+
+import org.example.demo3.controller.request.CreateTodoRequest
+import org.springframework.stereotype.Service
+
+@Service
+class TodoService {
+    fun createTodo(request: CreateTodoRequest): Boolean{
+
+
+        return false
+    }
+}
+
